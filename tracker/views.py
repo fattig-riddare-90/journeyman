cd44ff5354fe2ef60fe84878b2ddf376f1580d89
riddaren <fredrikjohansson793@live.se>
2025-06-29 15:35:21 +0200

    Add login required to views

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import DiaryEntry
 
 # Create your views here.
+
+@login_required
+def public_entries(request):
+    entries = DiaryEntry.objects.filter(is_public=True).exclude(user=request.user)
+    return render(request, 'tracker/my_entries.html', {'entries': entries})
+
+@login_required
+def my_entries(request):
+    entries = DiaryEntry.objects.filter(user=request.user)
+    return render(request, 'tracker/my_entries.html', {'entries': entries})
