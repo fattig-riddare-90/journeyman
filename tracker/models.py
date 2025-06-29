f15bcabbfcddbc56357dea6909612cc363d8d9fb
riddaren <fredrikjohansson793@live.se>
2025-06-29 14:53:19 +0200

    Add diary entry to models.py

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class DiaryEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    content = models.TextField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.date}"
