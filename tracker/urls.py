7ea14360d9e1ee145cd1928d857f1ebd74dc7190
riddaren <fredrikjohansson793@live.se>
2025-06-29 15:45:43 +0200

    Add entry paths to tracker/urls.py

---
+++ tracker/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns [
+    path('public/', views.public_entries, name='public_entries'),
+    path('my/', views.my_entries, name='my_entries'),
+]
