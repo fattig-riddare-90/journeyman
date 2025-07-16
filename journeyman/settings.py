ea028aeff0869be7620c4195dcf3fa5ab2fdc9e7
riddaren <fredrikjohansson793@live.se>
2025-07-17 01:42:55 +0200

    Add readme document

--- journeyman/settings.py
+++ journeyman/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
 
