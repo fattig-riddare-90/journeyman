41b055ae57f93257bcd06786c488c7f09c4d17c2
riddaren <fredrikjohansson793@live.se>
2025-07-17 02:53:36 +0200

    Add to user/forms.py in order to remove help_text to avoid validation error

--- users/forms.py
+++ users/forms.py
@@ -6,3 +6,9 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Remove help texts for all fields to avoid aria-describedby references
+        for field in self.fields.values():
+            field.help_text = None
