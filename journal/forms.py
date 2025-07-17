c3843d1a7347eec071934d81b56b4c91eab4dd24
riddaren <fredrikjohansson793@live.se>
2025-07-17 02:15:58 +0200

    Fix placeholder error on date in create_entry.html

--- journal/forms.py
+++ journal/forms.py
@@ -20,5 +20,6 @@ class DiaryEntryForm(forms.ModelForm):
                 field.widget.attrs['class'] = 'form-check-input'
             else:
                 field.widget.attrs['class'] = 'form-control'
-                field.widget.attrs['placeholder'] = f'Enter {field.label.lower()}'
+                if field_name != 'date':
+                    field.widget.attrs['placeholder'] = f'Enter {field.label.lower()}'
     
